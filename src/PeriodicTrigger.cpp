b74b8f39b4dd2863d78a44ae9b12b42a5d231f02
larsmartinjensvoll <larsmartin.jensvoll@gmail.com>
2024-05-25 18:43:46 +0200

    Updated the PeriodicTrigger class to use the new Timer class. Also added a sample userConfig.h file to the src directory. This file should be copied to the include directory and modified to suit the user's needs.

---
+++ src/PeriodicTrigger.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file PeriodicTrigger.cpp
+ *
+ * @brief Programmable timer interval
+ *
+ */
+
+#include "PeriodicTrigger.h"
+
+#include <Arduino.h>
+
+PeriodicTrigger::PeriodicTrigger(unsigned long ms) {
+    m_triggerInterval = ms;
+    m_tref = millis();
+}
+
+bool PeriodicTrigger::check() {
+    if ((millis() - m_tref) > m_triggerInterval) {
+        m_tref += m_triggerInterval;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void PeriodicTrigger::reset() {
+    m_tref = millis();
+}
+
+void PeriodicTrigger::reset(unsigned long ms) {
+    m_triggerInterval = ms;
+    m_tref = millis();
+}
